2ade2364edb39b44fd6de6eebe66f1dfda32c5ea
Arpit1033 <lcb2022015@iiitl.ac.in>
2024-02-13 10:27:59 +0530

    This commit introduces the ability for users to edit their profile information, including their first name, last name, email address, and profile photo. The functionality includes updating the user's information and replacing the existing profile photo with a new one. The changes are implemented across the views, templates, and forms to ensure a seamless user experience. Additionally, appropriate redirects and error handling are included to enhance usability and robustness.

--- socialmediaproject/users/forms.py
+++ socialmediaproject/users/forms.py
@@ -1,6 +1,17 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name','last_name','email')
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('photo',)
+        
 class LoginForm(forms.Form):
     username = forms.CharField()
     # widget : Mask all the characters which will be typed in that form
