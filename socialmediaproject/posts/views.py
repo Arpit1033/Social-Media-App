e831af56253dcb65ba02d3b873b82e8681d860a8
Arpit1033 <lcb2022015@iiitl.ac.in>
2024-02-18 11:21:34 +0530

    Added a feed page where user will be able to see the latest posts from different users. Also added basic tailwind styling to home page and password change page.

--- socialmediaproject/posts/views.py
+++ socialmediaproject/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import PostCreateForm
 from django.contrib.auth.decorators import login_required
+from .models import Post
 
 # Create your views here.
 
@@ -15,4 +16,7 @@ def post_create(request):
     else:
         form = PostCreateForm(data=request.GET)
     return render(request,'posts/create.html',{'form':form})
-        
+
+def feed(request):
+    posts = Post.objects.all()
+    return render(request,'posts/feed.html',{'posts':posts})
