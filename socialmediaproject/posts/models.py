8d9969bd0405285a83e2c0c8c3e468ea22da3562
Arpit1033 <lcb2022015@iiitl.ac.in>
2024-03-11 17:05:41 +0530

    Added comment functionality.

--- socialmediaproject/posts/models.py
+++ socialmediaproject/posts/models.py
@@ -26,8 +26,9 @@ class Post(models.Model):
         
 class Comment(models.Model):
     post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
-    body = models.TextField()
+    body = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now = True)
+    posted_by = models.CharField(max_length=100)
     
     # For ordering the comments according to the dates they have been created
     class Meta:
